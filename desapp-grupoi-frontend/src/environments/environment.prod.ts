a39fc547b0c33560d6562c19cb8f76bc6bf3dcc4
agusgs <garciasmithagustin@gmail.com>
2018-06-18 08:34:09 -0300

    fix api audience

--- desapp-grupoi-frontend/src/environments/environment.prod.ts
+++ desapp-grupoi-frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   authClientID: 'p8686zzvDeoUO4jEHL35MqbPRgdav8N8',
   authDomain: 'unq-desa-grupoi.auth0.com',
   authResponseType: 'token id_token',
-  authAudience: 'https://desap.herokuapp.com',
+  authAudience: 'http://localhost:9090',
   authRedirectUri: 'https://desap.herokuapp.com/frontend/auth',
   authScope: 'openid email profile',
   backendUrl: 'https://desap.herokuapp.com/'
